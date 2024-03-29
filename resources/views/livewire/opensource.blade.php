80a1bd055e2562867e26945e6aa01613bcc919ec
I Wayan Rupadana <rupadanawayan@gmail.com>
2024-03-29 18:18:18 +0800

    feat: codecrafters website

--- resources/views/livewire/opensource.blade.php
+++ resources/views/livewire/opensource.blade.php
@@ -4,7 +4,7 @@
       <h2 class="text-3xl font-bold text-gray-800 md:text-4xl dark:text-white">Open Source Packages</h2>
     </div>
     <div class="xl:container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-      <div class="grid gap-12 md:gap-6 md:grid-cols-2 lg:gap-12">
+      <div class="grid gap-12 md:gap-6 md:grid-cols-3 lg:gap-12">
         @foreach($posts as $post)
           <x-opensource-post :post='$post' />
         @endforeach
